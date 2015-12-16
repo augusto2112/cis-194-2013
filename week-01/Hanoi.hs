dc269de2c6d9c2abad3d9fe0175a845b6dee55c5
Agustin Camino <agustin.camino@gmail.com>
2015-12-16 06:34:51 -0500

    Add towers of Hanoi solution

---
+++ week-01/Hanoi.hs
@@ -0,0 +1,11 @@
+{-# OPTIONS_GHC -Wall #-}
+
+module Hanoi where
+
+-- Exercise 05
+type Peg = String
+type Move = (Peg, Peg)
+
+hanoi :: Int -> Peg -> Peg -> Peg -> [Move]
+hanoi 0 _ _ _ = []
+hanoi n a b c = hanoi (n - 1) a c b ++ [(a, b)] ++ hanoi (n - 1) c b a
