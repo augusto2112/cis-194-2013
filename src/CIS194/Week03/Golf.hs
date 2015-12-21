801efac6f3f6dee71a92f404ad889eeb2e8c42af
Agustin Camino <agustin.camino@gmail.com>
2015-12-20 23:37:37 -0500

    Add local maxima solution

--- src/CIS194/Week03/Golf.hs
+++ src/CIS194/Week03/Golf.hs
@@ -9,3 +9,12 @@ nth :: Int -> [a] -> [a]
 nth _ [] = []
 nth 1 xs = xs
 nth n xs = take 1 (drop (n - 1) xs) ++ nth n (drop n xs)
+
+-- Exercise 02
+localMaxima :: [Int] -> [Int]
+localMaxima []       = []
+localMaxima [x]      = []
+localMaxima (x:y:[]) = [] -- Equivalent to [x,y]
+localMaxima (x:y:z:zs)
+  | y > x && y > z = y : localMaxima (y:z:zs)
+  | otherwise      =     localMaxima (y:z:zs)
--- test/CIS194/Week03/GolfSpec.hs
+++ test/CIS194/Week03/GolfSpec.hs
@@ -11,3 +11,9 @@ spec = do
       skips "hello!" `shouldBe` ["hello!", "el!", "l!", "l", "o", "!"]
       skips [1]      `shouldBe` [[1]]
       skips [True, False] `shouldBe` [[True, False], [False]]
+
+  describe "localMaxima" $
+    it "finds a local maxima of a given list" $ do
+      localMaxima [2,9,5,6,1] `shouldBe` [9,6]
+      localMaxima [2,3,4,1,5] `shouldBe` [4]
+      localMaxima [1,2,3,4,5] `shouldBe` []
