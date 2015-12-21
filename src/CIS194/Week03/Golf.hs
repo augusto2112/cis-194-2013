7b18698be05c206d47aabc0e9d1481c400fbdafd
Agustin Camino <agustin.camino@gmail.com>
2015-12-20 23:39:58 -0500

    Refactor localMaxima to use a more generalized pattern matching

--- src/CIS194/Week03/Golf.hs
+++ src/CIS194/Week03/Golf.hs
@@ -12,9 +12,7 @@ nth n xs = take 1 (drop (n - 1) xs) ++ nth n (drop n xs)
 
 -- Exercise 02
 localMaxima :: [Int] -> [Int]
-localMaxima []       = []
-localMaxima [x]      = []
-localMaxima (x:y:[]) = [] -- Equivalent to [x,y]
 localMaxima (x:y:z:zs)
   | y > x && y > z = y : localMaxima (y:z:zs)
   | otherwise      =     localMaxima (y:z:zs)
+localMaxima _ = []
