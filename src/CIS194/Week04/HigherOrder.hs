7b03019919b711e6821fcb12639cceac341585cc
Agustin Camino <agustin.camino@gmail.com>
2015-12-28 07:28:19 -0500

    Add solution for foldTree

--- src/CIS194/Week04/HigherOrder.hs
+++ src/CIS194/Week04/HigherOrder.hs
@@ -3,3 +3,26 @@ module CIS194.Week04.HigherOrder where
 -- Exercise 01
 fun1 :: [Int] -> Int
 fun1 = product . map (\x -> x - 2) . filter even
+
+type Height = Int
+
+data Tree a = Leaf
+            | Node Height (Tree a) a (Tree a)
+  deriving (Show, Eq)
+
+foldTree :: [a] -> Tree a
+foldTree = foldr insert Leaf
+
+insert :: a -> Tree a -> Tree a
+insert val Leaf = Node 0 Leaf val Leaf
+insert val (Node ht lt root rt)
+  | height lt <= height rt =
+  let left = insert val lt
+  in Node (height left + 1) left root rt
+  | otherwise =
+  let right = insert val rt
+  in Node (height right + 1) lt root right
+
+height :: Tree a -> Int
+height Leaf = -1
+height (Node ht _ _ _) = ht
--- test/CIS194/Week04/HigherOrderSpec.hs
+++ test/CIS194/Week04/HigherOrderSpec.hs
@@ -9,3 +9,9 @@ spec = do
     it "computes a single value for a given list" $ do
       fun1 [10, 3, 1] `shouldBe` 8
       fun1 [7, 4, 5]  `shouldBe` 2
+
+  describe "foldTree" $ do
+    it "folds a tree for a given list" $ do
+      foldTree ['A']      `shouldBe` Node 0 Leaf 'A' Leaf
+      foldTree ['A'..'C'] `shouldBe`
+        Node 1 (Node 0 Leaf 'B' Leaf) 'C' (Node 0 Leaf 'A' Leaf)
