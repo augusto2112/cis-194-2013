11149ea04ce14a8c50d85da327639c3cf1b28b82
Agustin Camino <agustin.camino@gmail.com>
2015-12-29 09:00:50 -0500

    Refactor histogram after code review

--- src/CIS194/Week03/Golf.hs
+++ src/CIS194/Week03/Golf.hs
@@ -1,5 +1,7 @@
 module CIS194.Week03.Golf where
 
+import Data.Bool
+
 -- Exercise 01
 skips :: [a] -> [[a]]
 skips xs = map nth [1..(length xs)]
@@ -10,22 +12,24 @@ skips xs = map nth [1..(length xs)]
 localMaxima :: [Int] -> [Int]
 localMaxima (x:y:z:zs)
   | x < y && y > z = y : localMaxima (y:z:zs)
-  | otherwise       =     localMaxima (y:z:zs)
-localMaxima _       = []
+  | otherwise      = localMaxima (y:z:zs)
+localMaxima _ = []
 
 -- Exercise 03
 histogram :: [Int] -> String
 histogram xs = (unlines . lines') xs ++ "==========\n0123456789\n"
 
 lines' :: [Int] -> [String]
-lines' xs = map (line freq) range
+lines' xs = map (line freq) boundaries
   where
-    freq  = frecuency xs
-    max   = maximum freq
-    range = [max, (max - 1)..1]
+    freq       = frecuency xs
+    max        = maximum freq
+    boundaries = [max, (max - 1)..1]
 
 line :: [Int] -> Int -> String
-line xs n = [if n <= x then '*' else ' ' | x <- xs]
+line xs n = map (bool ' ' '*' . (n <=)) xs
 
 frecuency :: [Int] -> [Int]
-frecuency xs = map (\n -> (length . filter (== n)) xs)[0..9]
+frecuency xs = map count [0..9]
+  where
+    count n = length $ filter (== n) xs
