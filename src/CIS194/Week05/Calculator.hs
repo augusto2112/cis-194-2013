89b74a5db2a8f125c170c513d8c2207bf37a85d7
Agustin Camino <agustin.camino@gmail.com>
2015-12-30 07:32:19 -0500

    Add solution for evalStr

--- src/CIS194/Week05/Calculator.hs
+++ src/CIS194/Week05/Calculator.hs
@@ -1,9 +1,17 @@
 module CIS194.Week05.Calculator where
 
 import CIS194.Week05.ExprT
+import CIS194.Week05.Parser
 
 -- Exercise 01
 eval :: ExprT -> Integer
 eval (Lit n)     = n
 eval (Add lt rt) = eval lt + eval rt
 eval (Mul lt rt) = eval lt * eval rt
+
+-- Exercise 02
+evalStr :: String -> Maybe Integer
+evalStr str = case parseExp Lit Add Mul str of
+                   Just exprT -> Just (eval exprT)
+                   Nothing    -> Nothing
+
--- test/CIS194/Week05/CalculatorSpec.hs
+++ test/CIS194/Week05/CalculatorSpec.hs
@@ -6,6 +6,11 @@ import Test.Hspec
 
 spec :: Spec
 spec = do
-  describe "eval" $ do
+  describe "eval" $
     it "computes the given expression" $
       eval (Mul (Add (Lit 2) (Lit 3)) (Lit 4)) `shouldBe` 20
+
+  describe "evalStr" $
+    it "evaluates a given string" $ do
+      evalStr "(2+3)*4" `shouldBe` Just 20
+      evalStr "(2+3)*"  `shouldBe` Nothing
