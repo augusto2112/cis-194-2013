6be4fa761e566924434c640072f25bcec65ff527
Agustin Camino <agustin.camino@gmail.com>
2016-01-11 11:03:01 -0500

    Add solution for streamToList

--- src/CIS194/Week06/Fibonacci.hs
+++ src/CIS194/Week06/Fibonacci.hs
@@ -9,6 +9,20 @@ fib n = fib (n - 1) + fib (n - 2)
 fibs1 :: [Integer]
 fibs1 = map fib [0..]
 
+-- Exercise 02
 -- The implementation was borrowed from:
 -- https://wiki.haskell.org/The_Fibonacci_sequence
 fibs2 = 0 : 1 : zipWith (+) fibs2 (tail fibs2)
+
+-- Exercise 03
+data Stream a = Cons a (Stream a)
+
+instance Show a => Show (Stream a) where
+  show = show . take 20 . streamToList
+
+streamToList :: Stream a -> [a]
+streamToList (Cons x xs) = x : streamToList xs
+
+-- Exercise 04
+streamRepeat :: a -> Stream a
+streamRepeat x = Cons x (streamRepeat x)
--- test/CIS194/Week06/FibonacciSpec.hs
+++ test/CIS194/Week06/FibonacciSpec.hs
@@ -13,3 +13,13 @@ spec = do
     it "computes the nth fibonacci number" $ do
       fibs2 !! 100 `shouldBe` 354224848179261915075
       fibs2 !! 150 `shouldBe` 9969216677189303386214405760200
+
+  describe "streamToList" $
+    it "converts a stream into a list" $
+      let stream = streamRepeat 10
+      in (take 2 . streamToList $ stream) `shouldBe` [10, 10]
+
+  describe "show" $
+    it "conveniently prints the first 20 elements in a stream" $
+      let stream = streamRepeat 10
+      in show stream `shouldBe` (show [10 | _ <- [1..20]])
